2028e3522b4349f13f3ed2933e6849d4c290f8b3
Vargos <ahmed_ehab707@yahoo.com>
2018-06-28 22:13:01 +0200

    Adding the phone input mask and handling selection

--- app/src/main/java/com/fallenleaf/notekeeper/phonefielddemo/CustomeField/PhoneField.java
+++ app/src/main/java/com/fallenleaf/notekeeper/phonefielddemo/CustomeField/PhoneField.java
@@ -2,9 +2,9 @@ package com.fallenleaf.notekeeper.phonefielddemo.CustomeField;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TextInputEditText;
-import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
@@ -17,59 +17,41 @@ import android.widget.Spinner;
 
 import com.fallenleaf.notekeeper.phonefielddemo.R;
 
+import ru.kolotnev.formattedittext.MaskedEditText;
+
 /**
  * Created by Ahmed Ehab on 6/27/2018.
  */
 public class PhoneField extends LinearLayout {
+    public static final String CODE = "CODE";
     Spinner flagSpinner;
-    TextInputEditText phoneTextField;
+    MaskedEditText phoneTextField;
     private View view;
     private int codeLength = -1;
     private TextWatcher phoneTextWatcher = new TextWatcher() {
-        private String realOldText;
-
-        //private int tempSelection;
 
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            //tempSelection = phoneTextField.getSelectionStart() - 1;
-            realOldText = s.toString();
+
         }
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-             //String tempString = s.toString();
-            //s = tempString.replaceAll(" ","");
-            /*if( s.length() % 3 == 0 ){
-                phoneTextField.setText(s + " ");
-                phoneTextField.setSelection(phoneTextField.
-                        getText().toString().length());
 
-            }*/
-
-            //makes the limit of numbers are 10
-            if( (s.length() - codeLength) > 10 ){
-                phoneTextField.setText(s.subSequence(0,s.length()-1));
-                phoneTextField.setSelection(phoneTextField.
-                        getText().toString().length());
-            }
         }
 
         @Override
         public void afterTextChanged(Editable s) {
-            Log.e("TEST", "onTextChanged: ");
-
-            //prevent the user from removing the code
-            if (!s.toString().startsWith(codeString)) {
-                String oldText = realOldText.replace(" ", ""); //phoneTextField.getText().toString().replace(" ", "");
-                if(codeLength != -1){
-                    oldText = oldText.substring(codeLength-1,oldText.length());
+            //Prevent the user from removing the code.
+            String newText = s.toString();
+            newText = newText.replaceAll("\\+","");
+            newText = newText.replaceAll(" ","");
+            if(!newText.startsWith(codeString)){
+                String oldText = "";
+                if(codeLength+1 <= newText.length()){
+                    oldText = newText.substring(codeLength+1,newText.length());
                 }
-
                 phoneTextField.setText(codeString + oldText);
-                phoneTextField.setSelection(phoneTextField.
-                        getText().toString().length());
-
             }
         }
     };
@@ -99,7 +81,6 @@ public class PhoneField extends LinearLayout {
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        //addView(view,0);
         flagSpinner = findViewById(R.id.phone_field_flag_spinner);
         phoneTextField = findViewById(R.id.phone_field_textInput);
 
@@ -108,7 +89,11 @@ public class PhoneField extends LinearLayout {
         }
     }
 
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
 
+    }
 
     public void setFlagSpinner(TypedArray flagImagesArray, String[] flagCodesArray){
         final CustomFlagAdapter adapter = new CustomFlagAdapter(getContext(),flagImagesArray,flagCodesArray);
@@ -117,7 +102,7 @@ public class PhoneField extends LinearLayout {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 //change the code of the phone text field
-                changeTheInputFieldCode((String) adapterView.getItemAtPosition(i) + " ");
+                changeTheInputFieldCode((String) adapterView.getItemAtPosition(i) ); //+ " "
             }
 
             @Override
@@ -132,30 +117,61 @@ public class PhoneField extends LinearLayout {
     }
 
     private void changeTheInputFieldCode(String codeSelected) {
-        String oldText = phoneTextField.getText().toString();
+        String oldText = phoneTextField.getText(true).toString();
         if(codeLength != -1){
-            oldText = oldText.substring(codeLength,phoneTextField.length());
+            oldText = oldText.substring(codeLength,oldText.length());
         }
+
         codeLength = codeSelected.length();
         codeString = codeSelected;
-        phoneTextField.setText(codeString + oldText);
+        String codeMask = "";
+        for(int i = 0; i <codeLength; i++){
+            codeMask += "9"; //add a number for code as a mask
+        }
+
+        String finalMask = "+" + codeMask + " 999 999 9999"; //Format the phone number
+        phoneTextField.setMask(finalMask);
+        phoneTextField.setText(codeString + oldText); // change the country code and keep old text
         phoneTextField.setSelection(phoneTextField.getText().toString().length());
     }
 
     public String getPhoneNumber(){
-        return phoneTextField.getText().toString();
+        return phoneTextField.getText(true).toString();
     }
-
-
 
     public Spinner getFlagSpinner() {
         return flagSpinner;
     }
 
 
-    public TextInputEditText getPhoneTextField() {
+    public MaskedEditText getPhoneTextField() {
         return phoneTextField;
     }
 
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state instanceof Parcelable) {
+            Bundle bundle = (Bundle) state;
+            state = bundle.getParcelable("super");
+            codeString = bundle.getString(CODE,"20");
+        }
+        super.onRestoreInstanceState(state);
+    }
+
+    @Nullable
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("super", super.onSaveInstanceState());
+        bundle.putString(CODE, codeString); //Save Country code
+
+        return bundle;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        phoneTextField.removeTextChangedListener(phoneTextWatcher);
+    }
 }
 
--- app/src/main/java/com/fallenleaf/notekeeper/phonefielddemo/MainActivity.java
+++ app/src/main/java/com/fallenleaf/notekeeper/phonefielddemo/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.fallenleaf.notekeeper.phonefielddemo.CustomeField.PhoneField;
 
@@ -24,9 +26,16 @@ public class MainActivity extends AppCompatActivity{
         phoneField = findViewById(R.id.activity_phone_filed);
         submitButton = findViewById(R.id.submit_button);
 
-        String[] codesArray = getResources().getStringArray(R.array.codesArray);
-        TypedArray flagImgs = getResources().obtainTypedArray(R.array.flag_images);
+        String[] codesArray = getResources().getStringArray(R.array.codesArray); //Codes
+        TypedArray flagImgs = getResources().obtainTypedArray(R.array.flag_images); //Flags
 
-        phoneField.setFlagSpinner(flagImgs,codesArray);
+        phoneField.setFlagSpinner(flagImgs,codesArray); //Add the flagImages and codes
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "The Phone Number Is: " + phoneField.getPhoneNumber(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
